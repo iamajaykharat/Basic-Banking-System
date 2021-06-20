3525c17866773e0e9e2800c0cafefaec5c505e00
Ajay Kharat <65547863+iamajaykharat@users.noreply.github.com>
2021-06-20 20:09:55 +0530

    final commit

---
+++ bank/index1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>The Spark Bank</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <?php include('navbar.php');?>
+  <div id="particles-js">
+
+    <section class="container heading-wrapper">
+      <h2>Welcome to</h2>
+      <h1>The Spark Bank</h1>
+      <p>Money transfer made easy. Transfer credit in flash...</p> 
+      <a href="./all_users.php">View users</a> 
+    </section>
+  </div>
+  <script type="text/javascript" src="particles.js"></script>
+  <script type="text/javascript" src="app.js"></script>
+</body>
+</html>
